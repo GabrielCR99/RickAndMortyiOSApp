ee6a849b4bfe57a96b5707eb0112034f679f994c
Roveri <gabriel.roveri7@gmail.com>
2023-07-10 11:05:28 -0300

    Start building out Location tab

--- RickAndMorty/Controllers/Core/RMLocationViewController.swift
+++ RickAndMorty/Controllers/Core/RMLocationViewController.swift
@@ -9,18 +9,32 @@ import UIKit
 
 /// Controller to show and search for Locations
 final class RMLocationViewController: UIViewController {
-
+    
+    private let primaryView = RMLocationView()
+    
+    private let viewModel = RMLocationViewViewModel()
     // MARK: - View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
         title = "Locations"
+        view.backgroundColor = .systemBackground
+        view.addSubview(primaryView)
+        addConstraints()
         addSearchButton()
     }
     
     private func addSearchButton() {
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(didTapShare))
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            primaryView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            primaryView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            primaryView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            primaryView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
     }
     
     @objc
